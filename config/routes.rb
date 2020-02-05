b95938e940627d71c1b3428ad5ecbfc08bdc5a4c
Atsushi-cook <sacook7@icloud.com>
2020-02-05 21:33:51 +0900

    個人アプリ２commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  resources :tweets, only: [:index, :new]
+  devise_for :users
+
+  post "likes/:tweet_id/create" => "likes#create"
+  post "likes/:tweet_id/destroy" => "likes#destroy"
+
+  root to: 'tweets#index'
+  namespace :tweets do
+    resources :searches, only: :index
+  end
+  resources :tweets do
+    resources :comments, only: :create
+  end
+  resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
