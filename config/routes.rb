10bbcb0602f35fb2052e45884cd261c4ca5d883c
Atsushi-cook <sacook7@icloud.com>
2020-02-11 11:15:56 +0900

    個人アプリ commit4

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   post "likes/:tweet_id/create" => "likes#create"
   post "likes/:tweet_id/destroy" => "likes#destroy"
 
+
   root to: 'tweets#index'
   namespace :tweets do
     resources :searches, only: :index
   end
   resources :tweets do
     resources :comments, only: :create
+    resources :relationships, only: [:create, :destroy]
     
   end
   resources :users, only: :show
